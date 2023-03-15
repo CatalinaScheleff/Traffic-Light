041bd50d5ab7b132c84527a7a02309e08fedbb9f
Catalina Scheleff Cáceres <catalina@MacBook-Pro-de-Catalina.local>
2023-03-15 13:33:15 -0300

    luz encendida

--- src/js/component/traficlight.jsx
+++ src/js/component/traficlight.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 const TraficLight = () => {
+	const [luz,setLuz] = useState(" selected");
+	// const [amarillo,setAmarillo] = useState("luzAmarilla");
+	// const [verde,setVerde] = useState("luzVerde");
 	return (
 		<div className="text-center">
 			<div className="poste"></div>
 			<div className="semaforo">
-				<div className="luzRoja"></div>
-				<div className="luzAmarilla"></div>
-				<div className="luzVerde selected"></div>
+			<div className="luzRoja"{luz}
+				onClick= { ()=> { 
+
+					setLuz (luz)
+				}}></div>
+				<div className={amarillo} 
+				onClick= { ()=> {
+					setAmarillo (amarillo+ " selected")
+				}}></div>
+				<div className={verde} 
+				onClick= { ()=> {
+					setVerde (verde+ " selected")
+				}}></div>
 			</div>
 		</div>
 	);
